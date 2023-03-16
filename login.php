e84c7b11e695645aa823deb660540ecba1496002
RUFOS-od <odetokirufos@gmail.com>
2023-03-16 16:06:50 +0100

    second commit

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset($_SESSION["user"])) {
-   header("Location: index.php");
+    header("Location: index.php");
 }
 ?>
 <!DOCTYPE html>
@@ -12,8 +12,11 @@ if (isset($_SESSION["user"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -21,8 +24,8 @@ if (isset($_SESSION["user"])) {
     <div class="container">
         <?php
         if (isset($_POST["login"])) {
-           $email = $_POST["email"];
-           $password = $_POST["password"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
             require_once "database.php";
             $sql = "SELECT * FROM user WHERE email = '$email'";
             $result = mysqli_query($conn, $sql);
@@ -33,22 +36,31 @@ if (isset($_SESSION["user"])) {
                     $_SESSION["user"] = "yes";
                     header("Location: index.php");
                     die();
-                }else{
+                } else {
                     echo "<div class='alert alert-danger'>Password does not match</div>";
                 }
-            }else{
+            } else {
                 echo "<div class='alert alert-danger'>Email does not match</div>";
             }
         }
         ?>
+        <figure class="text-center">
+            <blockquote class="blockquote">
+                <p>Training PHP</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+                Organisez par la CVC <cite title="Source Title">Formation dispenser par Rufos</cite>
+            </figcaption>
+        </figure>
+
         <form action="login.php" method="post">
-            <div class="form-group">
+            <div class="mb-3">
                 <input type="email" placeholder="Enter Email:" name="email" class="form-control">
             </div>
-            <div class="form-group">
+            <div class="mb-3">
                 <input type="password" placeholder="Enter Password:" name="password" class="form-control">
             </div>
-            <div class="form-btn">
+            <div class="mb-3">
                 <input type="submit" value="Login" name="login" class="btn btn-primary">
             </div>
         </form>
